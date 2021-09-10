f3f1d59603611db0a871a4671caa8306fdc422f4
Marcos Moreira Junior <marcos.moreira.jr@gmail.com>
2021-09-10 13:26:40 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -23,14 +23,8 @@ const DonutChart = () => {
                 setChatData({ labels: myLabels, series: mySeries });
 
             });
-    }, [])
+    }, []);
 
-
-
-    // const mockData = {
-    //     series: [477138, 499928, 444867, 220426, 473088],
-    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    // }
 
     const options = {
         legend: {
